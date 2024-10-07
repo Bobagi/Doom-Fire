500c8efb610742dae2a1623623254ff943b1ae85
GUSTAVO ANTONIO PERIN <gustavoperin067@gmail.com>
2024-10-07 13:32:56 -0300

    feat: dimentions manipulations on running

--- src/Fire.js
+++ src/Fire.js
@@ -3,18 +3,20 @@ let fireWidth = 40
 let fireHeight = 40
 const firePalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 let debug = false
+let cellSize = 8;
 
-function start(){
+function start() {
 	fireDataStructure()
 	renderFire()
 	setInterval(firePropagation,50)
 }
 
-function fireDataStructure(){
+function fireDataStructure() {
 	const arrayLenght = fireWidth * fireHeight
 
-	for(let i = 0; i < arrayLenght; i++){
-		firePixel[i] = 0
+	for (let i = 0; i < arrayLenght; i++){
+		if(!firePixel[i]) 
+			firePixel[i] = 0
 	}
 
 	fireSource()
@@ -45,15 +47,14 @@ function renderFire(){
 			const colorString = `${color.r},${color.g},${color.b}`
 			if(debug){
 				html+='<td>'
-				//indice da celula no canto superior direito
+				// indice da celula no canto superior direito
 				html+=`<div class="pixel-index">${pixelIndex}</div>`
 
-				//intensidade de fogo no centro da celula
+				// intensidade de fogo no centro da celula
 				html+=`<div style="color: rgb(${colorString});">${firePixel[pixelIndex]}</div>`
 			}else{	
-
-				html+=`<td class="pixel" style="background-color: rgb(${colorString})">`
-
+				// Atualizando o tamanho da célula com base no slider
+				html+=`<td class="pixel" style="background-color: rgb(${colorString}); width: ${cellSize}px; height: ${cellSize}px;">`
 			}
 
 			html+='</td>'
@@ -78,17 +79,32 @@ function fireSource(){
 function debugMode(){
 	if(debug){
 		debug = false;
-		fireWidth = 40
-		fireHeight = 40
-
-	}else {
+	} else {
 		debug = true;
-		fireWidth = 10
-		fireHeight = 10
 	}
 
 	fireDataStructure()
 	renderFire()
+}
+
+// Funções para atualizar o tamanho da largura e altura dinamicamente
+function updateWidth(newWidth) {
+	fireWidth = parseInt(newWidth, 10);
+	document.getElementById('widthValue').innerText = newWidth;
+	fireDataStructure();
+}
+
+function updateHeight(newHeight) {
+	fireHeight = parseInt(newHeight, 10);
+	document.getElementById('heightValue').innerText = newHeight;
+	fireDataStructure();
+}
+
+// Função para atualizar o tamanho das células dinamicamente
+function updateCellSize(newSize) {
+    cellSize = parseInt(newSize, 10);  // Convertendo string para inteiro
+    document.getElementById('cellSizeValue').innerText = newSize;
+    renderFire();  // Re-renderizando o fogo com o novo tamanho das células
 }
 
 start()
